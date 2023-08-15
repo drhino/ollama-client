57874bcebd8cfa8712e3fb34ebe33ddc4308b723
Gerd <2538708+drhino@users.noreply.github.com>
2023-08-15 16:30:09 +0200

    worker-cron: sync registry

--- src/registryModels.js
+++ src/registryModels.js
@@ -276,6 +276,7 @@ export const registryModels = {
         "70b-v3-q3_K_S",
         "70b-v3-q4_K_M",
         "70b-v3-q4_K_S",
+        "70b-v3-q5_0",
         "70b-v3-q5_K_M",
         "70b-v3-q5_K_S",
         "70b-v3-q6_K",
