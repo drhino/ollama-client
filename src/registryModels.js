4ba5cf6516c6f459bc67e3438e3e4ca27dba5815
Gerd <2538708+drhino@users.noreply.github.com>
2023-08-26 01:54:06 +0200

    worker-cron: sync registry

--- src/registryModels.js
+++ src/registryModels.js
@@ -763,7 +763,11 @@ export const registryModels = {
         "13b-instruct-q5_1",
         "13b-instruct-q8_0",
         "13b-instruct-q3_K_L",
-        "13b-instruct-q4_K_S"
+        "13b-instruct-q4_K_S",
+        "13b-instruct-q4_K_M",
+        "13b-instruct-q5_K_M",
+        "13b-instruct-q5_K_S",
+        "13b-instruct-q6_K"
     ],
     "pdevine/mario": [
         "latest"
